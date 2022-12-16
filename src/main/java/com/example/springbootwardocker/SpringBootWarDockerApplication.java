b3a406fe78e59f31a87bb0362ca4907003976831
Suraj <suraj.kumar@arittek.com>
2022-12-16 15:25:26 +0500

    Initial Code Setup

---
+++ src/main/java/com/example/springbootwardocker/SpringBootWarDockerApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootwardocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootWarDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWarDockerApplication.class, args);
+	}
+
+}
